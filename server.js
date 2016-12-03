f79e55b9d7d0c648694eca0ef4f5ec6df3a2e331
Hunt Norment <hunt@sherpa.red>
2016-12-03 09:00:14 -0600

    skeleton routes

--- server.js
+++ server.js
@@ -4,39 +4,71 @@
 var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
-
 var mongodb = require('mongodb');
-var ObjectID = mongod.ObjectID;
+var ObjectID = mongodb.ObjectID;
 
 var CONTACTS_COLLECTION = "contacts";
 
 var app = express();
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
 
 // Create a database variable outside of the database connection callback to reuse the connection pool
 var db;
 
-// Connect to the database before starting the application server
-mongodb.MongoClient.connect(process.env.MONGODB_URI, function(err, database) {
+// Connect to the database before starting the application server.
+mongodb.MongoClient.connect(process.env.MONGODB_URI, function (err, database) {
     if (err) {
         console.log(err);
         process.exit(1);
     }
 
-    // Save database object from the callback for reuse
+    // Save database object from the callback for reuse.
     db = database;
     console.log("Database connection ready");
 
-    // Initialize the app
-    var server = app.listen(process.env.PORT || 8080, function() {
-       var port = server.address().port;
-       console.log("App is now running on port", port);
+    // Initialize the app.
+    var server = app.listen(process.env.PORT || 8080, function () {
+        var port = server.address().port;
+        console.log("App now running on port", port);
     });
 });
 
-
 // CONTACTS API ROUTES BELOW
 
-module.exports = app;
+// Generic error handler used by all endpoints
+function handleError(res, reason, message, code) {
+    console.log("ERROR" + reason);
+    res.status(code || 500).json({"error": message});
+}
+
+/* "/contacts"
+  *     GET: finds all contacts
+  *     POST: creates a new contact
+ */
+
+app.get("/contacts", function(req, res) {
+
+});
+
+app.post("/contacts", function(req, res) {
+
+});
+
+/* "contacts/:id"
+  *     GET: find contact by id
+  *     PUT: update contact by id
+  *     DELETE: deletes contact by id
+ */
+
+app.get("/contacts/:id", function(req, res) {
+
+});
+
+app.put("/contacts/:id", function(req, res) {
+
+});
+
+app.delete("/contacts/:id", function(req, res) {
+
+});
