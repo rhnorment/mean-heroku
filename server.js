e5d60ebe0b1694247eefc50509d3094473b34813
Hunt Norment <hunt@sherpa.red>
2016-12-03 09:54:00 -0600

    separating routes

--- server.js
+++ server.js
@@ -7,11 +7,13 @@ var bodyParser = require('body-parser');
 var mongodb = require('mongodb');
 var ObjectID = mongodb.ObjectID;
 
+var contacts = require('./routes/contacts');
 var CONTACTS_COLLECTION = "contacts";
 
 var app = express();
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
+app.use('/contacts', contacts);
 
 // Create a database variable outside of the database connection callback to reuse the connection pool
 var db;
@@ -32,70 +34,4 @@ mongodb.MongoClient.connect(process.env.MONGODB_URI, function(err, database) {
         var port = server.address().port;
         console.log("App now running on port", port);
     });
-});
-
-// CONTACTS API ROUTES BELOW
-
-// Generic error handler used by all endpoints
-function handleError(res, reason, message, code) {
-    console.log("ERROR" + reason);
-    res.status(code || 500).json({"error": message});
-}
-
-/* "/contacts"
-  *     GET: finds all contacts
-  *     POST: creates a new contact
- */
-
-app.get("/contacts", function(req, res) {
-    db.collection(CONTACTS_COLLECTION).find(
-        {}
-    ).toArray(function(err, docs) {
-        if (err) {
-            handleError(res, err.message, "Failed to get contacts.");
-        } else {
-            res.status(200).json(docs);
-        }
-    });
-});
-
-app.post("/contacts", function(req, res) {
-    var newContact = req.body;
-    newContact.createDate = new Date();
-
-    if (!(req.body.firstName || req.body.lastName)) {
-        handleError(res, "Invalid user input", "Must provide a first or last name.", 400);
-    }
-
-    db.collection(CONTACTS_COLLECTION).insertOne(newContact, function(err, doc) {
-       if (err) {
-           handleError(res, err.message, "Failed to create new contact.");
-       } else {
-           res.status(201).json(doc.ops[0]);
-       }
-    });
-});
-
-/* "contacts/:id"
-  *     GET: find contact by id
-  *     PUT: update contact by id
-  *     DELETE: deletes contact by id
- */
-
-app.get("/contacts/:id", function(req, res) {
-    db.collection(CONTACTS_COLLECTION).findOne({_id: new ObjectID(req.params.id)}, function(err, doc) {
-        if (err) {
-            handleError(res, err.message, "Failed to get contact.");
-        } else {
-            res.status(200).json(doc);
-        }
-    });
-});
-
-app.put("/contacts/:id", function(req, res) {
-
-});
-
-app.delete("/contacts/:id", function(req, res) {
-
 });
